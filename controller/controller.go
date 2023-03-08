b0d96770ae616d44ab98cb15687ed5fb7f1d3bbc
wangbenjiu <benjiu@6estates.com>
2023-03-08 17:43:28 +0800

    add

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"bubble/dao"
 	"bubble/models"
 	"net/http"
 
@@ -14,17 +13,33 @@ func IndexHandlers(c *gin.Context) {
 func CreateTodo(c *gin.Context) {
 	var todo models.Todo
 	c.BindJSON(&todo)
-	dao.DB.Create(&todo)
+	//dao.DB.Create(&todo)
+	err := models.CreateATodo(&todo)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, todo)
+
+	}
+
 }
 
 func GetTodoList(c *gin.Context) {
 	var todolist []*models.Todo
-	dao.DB.Find(&todolist)
-	c.JSON(http.StatusOK, &todolist)
+	todolist, err := models.GetAllTodo()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, todolist)
+	}
+	//dao.DB.Find(&todolist)
+	//c.JSON(http.StatusOK, &todolist)
 }
 
 func UpdateATodo(c *gin.Context) {
-
+	Id := c.Query("id")
 }
 
 func DeleteATodo(c *gin.Context) {
--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,20 @@
 package models
 
+import "bubble/dao"
+
 type Todo struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Status bool   `json:"status"`
 }
+
+func CreateATodo(todo *Todo) (err error) {
+	err = dao.DB.Create(&todo).Error
+	return
+}
+func GetAllTodo() (todolist []*Todo, err error) {
+	err = dao.DB.Find(&todolist).Error
+	return
+}
+
+func UpdateATodo
